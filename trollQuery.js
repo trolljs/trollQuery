82f2f030dede425987c4e7390660cc241a84c715
Mike Morici <Michael.Morici@cfpb.gov>
2015-01-30 15:54:51 -0500

    Adds addClass function.

--- trollQuery.js
+++ trollQuery.js
@@ -1,7 +1,14 @@
 (function(){
+  // jQuery.fn.text
   var jqText = jQuery.fn.text;
   jQuery.fn.text = function(){
     arguments[0] = 'Poop ' + arguments[0];
     jqText.apply( this, arguments );
-  }
+  };
+  // jQuery.fn.addClass
+  var jqAddClass = jQuery.fn.addClass;
+  jQuery.fn.addClass = function(){
+    arguments[0] = 'poop ' + arguments[0];
+    jqAddClass.apply( this, arguments );
+  };
 })();
